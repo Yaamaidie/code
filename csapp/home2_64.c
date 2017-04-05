6a42b10747080955e83de2038ab432fb4da5e75d
Yaamaidie <lijun_bigface@163.com>
2017-04-04 20:32:17 -0700

    change

--- csapp/home2_64.c
+++ csapp/home2_64.c
@@ -2,5 +2,5 @@
 /* Return 1 when any odd bit of x equals 1; 0 otherwise */
 int any_odd_one(unsigned x);
 int any_odd_one(unsigned x) {
-	return 0x55555555 & x == 0x55555555; /* assume x is 32 bytes*/
+	return 0xaaaaaaaa & x == 0xaaaaaaaa; /* assume x is 32 bytes*/
 }
